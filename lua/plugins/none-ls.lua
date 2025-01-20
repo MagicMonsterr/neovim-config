b76511dfb5fb2b1490bcca601bb999c5ecfc40b8
MagicMonsterr <magicmonsterdev@gmail.com>
2025-01-19 21:49:40 -0500

    Changed to clang_format for C++ and JS formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,7 @@ return {
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.clang_format,
             },
         })
         vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
