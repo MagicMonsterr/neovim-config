e529cec6348cac13aaec1a7d05dae1bfa412397c
MagicMonsterr <magicmonsterdev@gmail.com>
2025-01-20 22:24:19 -0500

    Added plugins and change keybinds

--- init.lua
+++ init.lua
@@ -8,5 +8,9 @@ vim.cmd("set expandtab")
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>qq", vim.cmd.qa)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>wq", vim.cmd.wqa)
+vim.keymap.set("n", "<C-Left>", vim.cmd.tabprevious)
+vim.keymap.set("n", "<C-Right>", vim.cmd.tabnext)
+vim.keymap.set("n", "<C-j>", vim.cmd.tabprevious)
+vim.keymap.set("n", "<C-k>", vim.cmd.tabnext)
+vim.keymap.set("n", "<C-q>", vim.cmd.tabclose)
